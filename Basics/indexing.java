443f8bf2d990a83162ad9928863594218472d59d
JASWANTHguruguntla <85709492+JASWANTHguruguntla@users.noreply.github.com>
2025-06-07 15:52:25 +0530

    Create indexing.java

---
+++ Basics/indexing.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class Indexing{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        String w=sc.nextLine();
+        int n=sc.nextInt();
+        System.out.println(w.charAt(n));
+    }
+}
