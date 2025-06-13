6be303cc646128efe63dcc5c0f221e983b64848b
JASWANTHguruguntla <85709492+JASWANTHguruguntla@users.noreply.github.com>
2025-06-13 21:36:57 +0530

    Create basicArithmetic.java

---
+++ OperatorsAndConditionalStatements/basicArithmetic.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class main{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+    }
+}
+/*
+Explanation
+In the given example, 
+the inputs are A =4 and B = 3 
+Addition: 4 +3 =7 
+Subtraction: 4-3=1 
+Multiplication: 4 * 3 =12
+Sample Input 1
+4
+3
+Sample Output 1
+7
+1 
+12
+Sample Input 2
+2
+5
+Sample Output 2
+7
+-3 
+10
+*/
