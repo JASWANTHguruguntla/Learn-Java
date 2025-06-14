662f69b82a3f90efd4d49c8fcd45721546e7953a
JASWANTHguruguntla <85709492+JASWANTHguruguntla@users.noreply.github.com>
2025-06-14 14:58:09 +0530

    Create lengthOfString.java

---
+++ OperatorsAndConditionalStatements/lengthOfString.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class LengthOfString{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        String line=sc.nextLine();
+        System.out.println(line.length());
+    }
+}
+
+/*
+Sample Input 1
+Software
+Sample Output 1
+8
+Sample Input 2
+Hello World
+Sample Output 2
+10
+*/
